64e586ac6c818d5edf55e6d2c13c0b23765060cc
Parkhyunwoo <parkhj0423@naver.com>
2023-02-19 20:45:50 +0900

    [parkhj0423] Initial Commit

---
+++ Animated-Sticky-Header/Animated-Sticky-Header/Animated_Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Sticky_HeaderApp.swift
+//  Animated-Sticky-Header
+//
+//  Created by 박현우 on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
